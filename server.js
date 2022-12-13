4611acbb19fd5e59d3d603c96d37c4932d3eee11
Andrey Avdeev <a3avdeev@gmail.com>
2022-12-13 17:54:01 +0200

    add third branch and database

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
+
+const DB_HOST =
+  "mongodb+srv://avdeev:AcPc6hMXynLr5d2w@cluster0.3brvvld.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
