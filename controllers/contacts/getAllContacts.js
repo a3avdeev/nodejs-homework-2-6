e38d356238de66fcfeebbda6403e5896e6791505
Andrey Avdeev <a3avdeev@gmail.com>
2023-01-02 18:00:23 +0200

    add filtration by favorite through  operator

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -3,13 +3,18 @@ const { Contact } = require("../../models/contacts");
 const getAllContacts = async (req, res, next) => {
   try {
     const { _id: owner } = req.user;
-    const { page = 1, limit = 10 } = req.query;
+    const { page = 1, limit = 10, favorite } = req.query;
     const skip = (page - 1) * limit;
 
-    const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
-      skip,
-      limit,
-    }).populate("owner", "name email");
+    const result = await Contact.find(
+      { owner, favorite: { $eq: favorite } },
+      "-createdAt -updatedAt",
+      {
+        skip,
+        limit,
+        favorite,
+      }
+    ).populate("owner", "name email");
 
     res.json(result);
   } catch (error) {
